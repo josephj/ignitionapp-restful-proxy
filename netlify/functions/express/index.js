4f5cdb574783de496e357a483e92be4cf5363161
Joseph Chiang <josephj6802@gmail.com>
2023-12-02 07:50:23 +1100

    Update

--- netlify/functions/express/index.js
+++ netlify/functions/express/index.js
@@ -1,56 +1,57 @@
-// index.js (renamed to express.js as per Netlify Functions structure)
-const express = require('express');
-const serverless = require('serverless-http');
-const { GraphQLClient } = require('graphql-request');
 const { useSofa } = require('sofa-api');
+const express = require('express');
 const { buildSchema } = require('graphql');
-const fs = require('fs');
-const path = require('path');
-
-const loadSchemaFromFile = () => {
-  const schemaPath = path.join('schema.graphql');
-  if (!fs.existsSync(schemaPath)) {
-    console.error(`The schema file was not found at ${schemaPath}`);
-    return null;
-  }
-  const sdl = fs.readFileSync(schemaPath, 'utf-8');
-  return buildSchema(sdl);
-}
-
+const { GraphQLClient } = require('graphql-request');
+const path = require("path");
+const fs = require("fs");
+const serverless = require("serverless-http");
 
 const app = express();
+
+const schemaPath = path.join('schema.graphql');
+if (!fs.existsSync(schemaPath)) {
+  console.error(`The schema file was not found at ${schemaPath}`);
+  return null;
+}
+const sdl = fs.readFileSync(schemaPath, 'utf-8');
+const schema = buildSchema(sdl);
+
 app.use(express.json());
 
-const schema = loadSchemaFromFile();
-
-const endpoint = 'https://demo.ignitionapp.com/graphql';
-const graphqlClient = new GraphQLClient(endpoint);
-
-useSofa({
-  basePath: '/api',
-  schema,
-  errorHandler(errors) {
-    console.error(errors);
-    return new Response(formatError(errs[0]), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  },
-  onRoute(info) {
-    app[info.method](info.path, async (req, res) => {
-      const accessToken = req.headers['authorization'] || 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ1c3J0a25fbXZ1dXRva2RsaGRxYWxhYWtqcWEiLCJpYXQiOjE3MDEzOTg5NjksImV4cCI6MTcwMTQwMDc2OSwic3ViIjoidXNlcl9tcDc3NnBvbWdnZXFhZ3FheGx3cSIsInNjb3BlcyI6WyJtZmEiLCJhcHAiXSwic2Vzc2lvbl9pZCI6IjIwZGM1ZWFjM2Q2N2RjNDdlYTZlZDk3MTE2Y2JhZTcyNzAyMTM1MDIwYmEyZTI4NmU4ZmJjMTdjMjIwMjZmYzgifQ.qCrq8yJoX2Fm1JekwO_Xe8O4E9__wBuM-LmBvPz7Lw0';
-      if (accessToken) {
-        graphqlClient.setHeader('authorization', `Bearer ${accessToken}`);
+app.use(
+  useSofa({
+    basePath: '/api',
+    schema: schema,
+    async execute({ contextValue, document, variables, ...rest }) {
+      // const authHeader = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ1c3J0a25fbXZ2ZWJwM2l1Z2hhYWxhYWllY3EiLCJpYXQiOjE3MDE0NjIyMDcsImV4cCI6MTcwMTQ2NDAwNywic3ViIjoidXNlcl9tcDc3NnBvbWdnZXFhZ3FheGx3cSIsInNjb3BlcyI6WyJtZmEiLCJhcHAiXSwic2Vzc2lvbl9pZCI6IjIxZTJlMjFkYzUwZmNkYmVkYmU2ZTY0YjMxMzRkMzBiOTg4NDJhNTY3Nzk2Yzk0YmE0YTQzODhlM2NhY2JkOGUifQ._drpPPVlEv11sntpDe3qQL0Dtv7Od4P8ni26Ofu2HJU';
+      const authHeader = contextValue.req.headers.authorization || ''
+      if (authHeader.indexOf('Bearer ') !== 0) {
+        const error = new Error('Unauthorized: No valid authorization header provided.');
+        return {
+          errors: [
+            {
+              message: error.message,
+              status: 401,
+              locations: [],
+            }
+          ]
+        };
       }
+
+      const graphqlClient = new GraphQLClient('https://demo.ignitionapp.com/graphql', {
+        headers: {
+          authorization: authHeader,
+        },
+      });
 
       try {
-        const data = await graphqlClient.request(info.operation, req.body);
-        res.send(data);
+        const data = await graphqlClient.request(document, variables);
+        return { data: data };
       } catch (error) {
-        res.status(500).send(error);
+        return { errors: [error] };
       }
-    });
-  },
-});
+    },
+  }),
+);
 
 module.exports.handler = serverless(app)
