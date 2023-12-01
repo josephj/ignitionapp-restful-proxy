47d0d89bada1349d90314fb565c8175f78f747a3
Joseph Chiang <josephj6802@gmail.com>
2023-12-02 07:57:15 +1100

    Update

--- netlify/functions/express/index.js
+++ netlify/functions/express/index.js
@@ -23,9 +23,8 @@ app.use(
     basePath: '/api',
     schema: schema,
     async execute({ contextValue, document, variables, ...rest }) {
-      // const authHeader = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ1c3J0a25fbXZ2ZWJwM2l1Z2hhYWxhYWllY3EiLCJpYXQiOjE3MDE0NjIyMDcsImV4cCI6MTcwMTQ2NDAwNywic3ViIjoidXNlcl9tcDc3NnBvbWdnZXFhZ3FheGx3cSIsInNjb3BlcyI6WyJtZmEiLCJhcHAiXSwic2Vzc2lvbl9pZCI6IjIxZTJlMjFkYzUwZmNkYmVkYmU2ZTY0YjMxMzRkMzBiOTg4NDJhNTY3Nzk2Yzk0YmE0YTQzODhlM2NhY2JkOGUifQ._drpPPVlEv11sntpDe3qQL0Dtv7Od4P8ni26Ofu2HJU';
       const authHeader = contextValue.req.headers.authorization || ''
-      if (authHeader.indexOf('Bearer ') !== 0) {
+      if (!authHeader.includes('Bearer')) {
         const error = new Error('Unauthorized: No valid authorization header provided.');
         return {
           errors: [
